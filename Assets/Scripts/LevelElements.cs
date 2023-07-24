e1204b29dc520c22530ea786fd3922a70ccbe832
aitanaceca <aitanaceca@gmail.com>
2023-07-25 00:12:17 +0200

    [FEAT] Añadida colocación random de elementos según el nivel en el que se encuentra el jugador.

--- Assets/Scripts/LevelElements.cs
+++ Assets/Scripts/LevelElements.cs
@@ -1,5 +1,8 @@
 using Scripts.Levels;
 using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Scripts.LevelElements
 {
@@ -8,39 +11,63 @@ namespace Scripts.LevelElements
         public int WaterMaterial { get; set; }
         public int SandMaterial { get; set; }
         public int FireMaterial { get; set; }
-        public int WoodMaterial { get; set; }
         public int GrassMaterial { get; set; }
+        public int WoodMaterial { get; set; }
 
-        private void SetMaterials(int waterMaterial, int sandMaterial, int fireMaterial, int woodMaterial, int grassMaterial)
+        private void SetMaterials(int waterMaterial, int sandMaterial, int fireMaterial, int grassMaterial, int woodMaterial)
         {
             WaterMaterial = waterMaterial;
             SandMaterial = sandMaterial;
             FireMaterial = fireMaterial;
-            WoodMaterial = woodMaterial;
             GrassMaterial = grassMaterial;
+            WoodMaterial = woodMaterial;
         }
 
-        public LevelElements(Level level)
+        public LevelElements(int level)
         {
-            Console.WriteLine("level", level);
             switch (level)
             {
-                case Level.ONE:
+                case (int)Level.ONE:
                     SetMaterials(4, 4, 3, 4, 4);
                     break;
-                case Level.TWO:
+                case (int)Level.TWO:
                     SetMaterials(3, 6, 2, 4, 4);
                     break;
-                case Level.THREE:
+                case (int)Level.THREE:
                     SetMaterials(3, 8, 2, 3, 3);
                     break;
-                case Level.FOUR:
+                case (int)Level.FOUR:
                     SetMaterials(2, 10, 1, 3, 3);
                     break;
                 default:
                     SetMaterials(4, 4, 3, 4, 4);
                     break;
             }
+        }
+
+        public string GetRandomElement()
+        {
+            Type classType = this.GetType();
+            PropertyInfo[] properties = classType.GetProperties();
+
+            List<string> propertiesNames = new() { };
+
+            foreach (PropertyInfo propertyInfo in properties)
+            {
+                var property = classType.GetProperty(propertyInfo.Name);
+
+                if ((int)property.GetValue(this) > 0)
+                {
+                    propertiesNames.Add(property.Name);
+                }
+            }
+
+            System.Random random = new();
+
+            // Genera un número aleatorio entre 0 y 4
+            int randomNumber = random.Next(0, propertiesNames.Count);
+
+            return propertiesNames[randomNumber];
         }
     }
 }
