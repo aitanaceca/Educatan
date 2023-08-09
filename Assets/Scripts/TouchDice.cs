85be281e4fa2d3692beaf07a49cefc87dcc704ce
aitanaceca <aitanaceca@gmail.com>
2023-08-09 21:19:50 +0200

    [REF] Eliminado código comentado.

--- Assets/Scripts/TouchDice.cs
+++ Assets/Scripts/TouchDice.cs
@@ -13,41 +13,6 @@ public class TouchDice : DefaultObserverEventHandler
     private List<string> diceFaces = new() { "Cara1", "Cara2", "Cara3", "Cara4", "Cara5", "Cara6" };
     private List<string> diceAnimations = new() { "dice1", "dice2", "dice3", "dice4", "dice5", "dice6" };
 
-    //void Start()
-    //{
-        //string faceName = face.name;
-        //switch (faceName)
-        //{
-        //    case "Cara1":
-        //        DiceNumber = 1;
-        //        break;
-
-        //    case "Cara2":
-        //        DiceNumber = 2;
-        //        break;
-
-        //    case "Cara3":
-        //        DiceNumber = 3;
-        //        break;
-
-        //    case "Cara4":
-        //        DiceNumber = 4;
-        //        break;
-
-        //    case "Cara5":
-        //        DiceNumber = 5;
-        //        break;
-
-        //    case "Cara6":
-        //        DiceNumber = 6;
-        //        break;
-
-        //    default:
-        //        print("Dado");
-        //        break;
-        //}
-    //}
-
     void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
