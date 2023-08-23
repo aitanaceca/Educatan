b2bb0b832ccf75e68cd5bf7ea774804d3b1e5629
aitanaceca <aitanaceca@gmail.com>
2023-08-23 20:47:24 +0200

    [FEAT] Animaciones de posibles movimientos finalizadas.

--- Assets/Scripts/TouchDice.cs
+++ Assets/Scripts/TouchDice.cs
@@ -66,45 +66,30 @@ public class TouchDice : DefaultObserverEventHandler
         "upElement1Trigger"
     };
 
-    private void ShowPossibleElements()
-    {
-        int diceNumberTextToInt = int.Parse(diceNumberText.text);
-        List<string> possiblePositions = new() { };
+    List<string> possiblePositions = new() { };
 
+    private List<string> ShowPossibleElements(List<string> possiblePositions, int diceNumberTextToInt)
+    {
         if (currentPossition - diceNumberTextToInt >= 0)
         {
             boardAnimator.SetTrigger(upElementsAnimationNames[currentPossition - diceNumberTextToInt]);
-            possiblePositions.Add(upElementsAnimationNames[currentPossition - diceNumberTextToInt]);
+            possiblePositions.Add(roadElements[currentPossition - diceNumberTextToInt]);
         }
 
         if (currentPossition + diceNumberTextToInt < roadElements.Count)
         {
             boardAnimator.SetTrigger(upElementsAnimationNames[currentPossition + diceNumberTextToInt]);
-            possiblePositions.Add(upElementsAnimationNames[currentPossition + diceNumberTextToInt]);
+            possiblePositions.Add(roadElements[currentPossition + diceNumberTextToInt]);
         }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (possiblePositions.Contains(hit.transform.name))
-                {
-                    // Mover personaje.
-                    // Actualizar currentPossition.
-                    // Parar animaciones de elementos que se levantan
-                    print("Esta posicion si se puede");
-                }
-            }
-        }
+        return possiblePositions;
     }
 
     void Update()
     {
         diceNumberText = diceNumber.GetComponent<TMP_Text>();
         diceNumberText.enabled = false;
+        int diceNumberTextToInt = 0;
 
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
@@ -117,7 +102,7 @@ public class TouchDice : DefaultObserverEventHandler
                 {
                     System.Random random = new();
 
-                    // Genera un n�mero aleatorio entre 0 y 5
+                    // Genera un numero aleatorio entre 0 y 5
                     int randomNumber = random.Next(0, diceAnimations.Count);
 
                     diceAnimator.Play(diceAnimations[randomNumber]);
@@ -125,8 +110,27 @@ public class TouchDice : DefaultObserverEventHandler
                     finalRandomNumber = randomNumber + 1;
                     diceNumberText.text = finalRandomNumber.ToString();
 
-                    // Bloquear dado cuando se termine la animaci�n del movimiento del propio dado.
-                    ShowPossibleElements();
+                    // TODO: Bloquear dado cuando se termine la animacion del movimiento del propio dado.
+
+                    diceNumberTextToInt = int.Parse(diceNumberText.text);
+                    possiblePositions = ShowPossibleElements(possiblePositions, diceNumberTextToInt);
+                }
+
+                if (possiblePositions.Contains(hit.transform.name))
+                {
+                    // TODO: Mover personaje.                    
+                    // Parar animaciones de elementos que se levantan
+                    foreach(var position in possiblePositions)
+                    {
+                        int index = roadElements.IndexOf(position);
+                        string triggerName = upElementsAnimationNames[index];
+                        string stopTrigger = triggerName + "Stop";
+                        boardAnimator.SetTrigger(stopTrigger);
+                    }
+                    
+                    // Actualizar currentPossition.
+                    currentPossition = roadElements.IndexOf(hit.transform.name);
+                    possiblePositions = new() { };
                 }
             }
         }
